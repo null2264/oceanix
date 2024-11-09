4c3b1c91ca9a59e7b4a69868a261b93307056f1e
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-09 16:16:28 +0700

    revert: "Adjust airportitlwm version format"
    This reverts commit be257ff51c0a89c46e4740658ab72b73d0925e49.

--- pkgs/kexts/airportitlwm/default.nix
+++ pkgs/kexts/airportitlwm/default.nix
@@ -9,13 +9,12 @@
         verInfo = import ./version.nix {
           inherit lib;
         };
-      in {
-      "${ver}" = (builtins.listToAttrs (map
-        (x: lib.attrsets.nameValuePair "${lib.strings.toLower x}" (pkgs.callPackage ./airportitlwm.nix {
+      in
+      builtins.listToAttrs (map
+        (x: lib.attrsets.nameValuePair "${ver}-${lib.strings.toLower x}" (pkgs.callPackage ./airportitlwm.nix {
           inherit ver;
           osVer = x;
         }))
-        verInfo."${ver}".supportedOS));
-      };
+        verInfo."${ver}".supportedOS);
   });
 }
