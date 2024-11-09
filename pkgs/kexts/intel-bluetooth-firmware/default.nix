5ebea4f5381036e964ef6d2d7cbad7890a59129f
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-09 15:45:45 +0700

    fix: Missing semi-colon

--- pkgs/kexts/intel-bluetooth-firmware/default.nix
+++ pkgs/kexts/intel-bluetooth-firmware/default.nix
@@ -2,11 +2,11 @@
 
 {
   intel-bluetooth-firmware = (import ../../pkger.nix {
-      inherit lib pkgs;
-      path = ./.;
-      fn = ver: {
-        "${ver}" =
-          pkgs.callPackage ./intel-bluetooth-firmware.nix { inherit ver; };
-      }
-    });
+    inherit lib pkgs;
+    path = ./.;
+    fn = ver: {
+      "${ver}" =
+        pkgs.callPackage ./intel-bluetooth-firmware.nix { inherit ver; };
+    };
+  });
 }
