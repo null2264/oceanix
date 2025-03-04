1afdeebb1d719e18a9693dc41c7113bc06fa04e1
Ahmad Ansori Palembani <palembani@gmail.com>
2025-03-04 08:40:39 +0700

    applemcereporterdisabler: fix build

--- pkgs/kexts/applemcereporterdisabler/default.nix
+++ pkgs/kexts/applemcereporterdisabler/default.nix
@@ -7,12 +7,12 @@ in {
     pname = "applemcereporterdisabler";
     version = "1.0.0";
 
-    src = fetchzip {
+    src = pkgs.fetchzip {
       url = "https://github.com/acidanthera/bugtracker/files/3703498/AppleMCEReporterDisabler.kext.zip";
       hash = "sha256-I1PcfchLqRXJAu5TEjK1wrgPL91pLCgEunxwlvhbueM=";
       stripRoot = false;
     };
 
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   });
 }
